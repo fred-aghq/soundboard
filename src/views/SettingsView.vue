80af0c297fcb3127cdecaffb319a0a2c3a83d6f2
Fred Spooner <fred@aghq.uk>
2023-03-09 14:22:26 +0000

    working with MIDI devices in a very basic way

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -3,28 +3,40 @@
 import InputDropdown from '@/components/Devices/InputDropdown.vue';
 import { useInputListStore } from '@/stores/inputListStore.js';
 import PrimaryTitle from '@/components/Global/atoms/PrimaryTitle.vue';
-import { ref } from 'vue';
+import { storeToRefs } from 'pinia';
 
 const inputListStore = useInputListStore();
 
-const inputs = inputListStore.inputs;
-let selectedDevice = inputListStore.currentInput;
+
+const { inputs, currentInput } = storeToRefs(inputListStore);
 
 function updateSelectedInput (event) {
     inputListStore.setCurrentInput(event.target.value);
-    console.log(inputListStore.currentInput);
+    console.log(currentInput);
+};
+
+const refresh = () => {
+    console.debug("refresh");
+    inputListStore.refresh();
 };
 </script>
 
 <template>
+    <button class="p-4 border border-gray-500" @click="refresh">
+        Refresh
+    </button>
     <p v-if="!inputs.length">
         No devices were found.
     </p>
 
     <PrimaryTitle class="pb-4">
-        Managing Device: {{ inputListStore.currentInput }}
+        Managing Device: {{ currentInput }}
     </PrimaryTitle>
 
     <InputDropdown @inputSelected="$event => updateSelectedInput($event)" :inputs="inputs" />
+
+    <pre>
+        {{  currentInput.id }}
+    </pre>
 </template>
  
