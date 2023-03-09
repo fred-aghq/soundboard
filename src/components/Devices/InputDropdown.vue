10d27d4eb9b6f44ffa0f84f6ab82c8e9903fe30a
Fred Spooner <fred@aghq.uk>
2023-03-09 13:19:19 +0000

    chore: InputDropdown light fix

--- src/components/Devices/InputDropdown.vue
+++ src/components/Devices/InputDropdown.vue
@@ -7,7 +7,7 @@ defineProps({
 </script>
 
 <template>
-    <select @change="$emit('inputSelected', $event)" v-if="inputs.length">
+    <select class="dark: bg-slate-600 bg-white border border-2 rounded-md p-1 pr-5" @change="$emit('inputSelected', $event)" v-if="inputs.length">
         <option v-for="input in inputs" :key="input.name" :value="input.name">
             {{ input.name }}
         </option>
