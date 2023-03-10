0631d8b0ab9896b65c9ea202b51b0449daa2f891
Fred Spooner <fred@aghq.uk>
2023-03-10 14:19:01 +0000

    chore: formatting

--- src/views/NavView.vue
+++ src/views/NavView.vue
@@ -1,13 +1,16 @@
 <script setup>
+import ActivityLight from '@/components/Devices/atoms/ActivityLight.vue';
 import { RouterLink } from 'vue-router'
+
 </script>
 <template>
     <nav class="flex justify-between items-center py-4">
         <h1 class="text-2xl font-bold">
-        <RouterLink to="/">My App</RouterLink>
+            <RouterLink to="/">My App</RouterLink>
         </h1>
         <div>
-        <RouterLink to="/settings" class="p-4">Settings</RouterLink>
+            <RouterLink to="/settings" class="p-4">Settings</RouterLink>
         </div>
+
     </nav>
 </template>
