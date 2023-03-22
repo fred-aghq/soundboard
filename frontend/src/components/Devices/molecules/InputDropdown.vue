e05061fd3640b929a3402c6f17504fb52a25bde6
Fred Spooner <fred@aghq.uk>
2023-03-22 17:52:47 +0000

    feat: disable dropdown if no device; bit clearer when user hits homepage

--- frontend/src/components/Devices/molecules/InputDropdown.vue
+++ frontend/src/components/Devices/molecules/InputDropdown.vue
@@ -5,7 +5,7 @@ import { storeToRefs } from 'pinia';
 const inputListStore = useInputListStore();
 
 
-const { inputs } = storeToRefs(inputListStore);
+const { inputs, inputCount } = storeToRefs(inputListStore);
 
 function updateSelectedInput(event) {
     inputListStore.setCurrentInput(event.target.value);
@@ -13,9 +13,19 @@ function updateSelectedInput(event) {
 </script>
 
 <template>
-    <select class="border border-2 rounded-md p-1 pr-5" @change="updateSelectedInput" v-if="inputs.length">
-        <option class="dark: bg-slate-600 light:bg-white" v-for="(input, _) in inputs" :key="_" :value="input.name">
-            {{ input.name }}
+    <select :disabled="inputCount < 1" class="border border-2 rounded-md p-1 pr-5" @change="updateSelectedInput">
+        <option 
+            :class="{
+                'dark: bg-slate-600 light:bg-white': true,
+            }"
+            v-for="(input, _) in inputs" 
+            :key="_" :value="input.name"
+            v-if="inputCount > 0"
+        >
+                {{ input.name }}
+        </option>
+        <option v-else>
+            No devices detected.
         </option>
     </select>
 </template>
